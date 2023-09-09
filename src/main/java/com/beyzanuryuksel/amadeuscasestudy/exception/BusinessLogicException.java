6004c5a1af8bc74629631743c793bbfa8ee03326
beyzayukseel <68306258+beyzayukseel@users.noreply.github.com>
2023-09-09 18:10:53 +0300

    feat: add general exception structure

---
+++ src/main/java/com/beyzanuryuksel/amadeuscasestudy/exception/BusinessLogicException.java
@@ -0,0 +1,29 @@
+package com.beyzanuryuksel.amadeuscasestudy.exception;
+
+
+public class BusinessLogicException {
+
+    public static class NotFoundException extends BaseException {
+        public NotFoundException(String message) {
+            super(message);
+        }
+    }
+
+    public static class AlreadyDeletedException extends BaseException {
+        public AlreadyDeletedException(String message) {
+            super(message);
+        }
+    }
+
+    public static class CanNotDeletedException extends BaseException {
+        public CanNotDeletedException(String message) {
+            super(message);
+        }
+    }
+
+    public static class NotValidException extends BaseException {
+        public NotValidException(String message) {
+            super(message);
+        }
+    }
+}
